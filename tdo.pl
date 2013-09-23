6d841d19db326eddc86bf56953b0dcc51a473937
Nathan Campos <eeepc904@gmail.com>
2013-09-23 12:41:27 -0300

    Crappy splitting

--- tdo.pl
+++ tdo.pl
@@ -4,12 +4,18 @@
 #
 # The easiest way to manage your TODOs.
 
+# TODO:
+#   - Have a global TODO list.
+#   - Check for local TODO lists.
+#   - Maybe: Read source code files for TODO comments.
+
 use strict;
 use warnings;
 use Data::Dumper;
 
 use Getopt::Long;
 use Term::ANSIColor;
+use Term::ReadLine;
 
 
 # Usage message.
@@ -19,3 +25,29 @@ sub usage {
 	print "Arguments:\n";
 	print "    -h\t\tThis message\n";
 }
+
+sub main {
+#	my $term = Term::ReadLine->new("tdo");
+	my $prompt = ": ";
+#	my $OUT = $term->OUT || \*STDOUT;
+
+	open(TODO, "<", "TODO") or die "Couldn't open TODO: $!";
+	while (my $line = <TODO>) {
+		chomp $line;
+
+		my @part = $line =~ /^(\s[-x]\s)|(.+)$/gi;
+		my $state = " ";
+		my $msg = $part[3];
+
+		# Check if the task is done.
+		if ($part[0] =~ /x/) {
+			$state = "x";
+		}
+
+		# Print the task.
+		print "    [$state] $msg\n";
+	}
+	close(TODO);
+}
+
+main();
