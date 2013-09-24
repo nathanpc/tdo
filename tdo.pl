8914eec0e800f80405d721fd06dd551466a49116
Nathan Campos <eeepc904@gmail.com>
2013-09-24 07:52:41 -0300

    Parsing and printing

--- tdo.pl
+++ tdo.pl
@@ -26,28 +26,81 @@ sub usage {
 	print "    -h\t\tThis message\n";
 }
 
-sub main {
-#	my $term = Term::ReadLine->new("tdo");
-	my $prompt = ": ";
-#	my $OUT = $term->OUT || \*STDOUT;
+sub parse_tasks {
+	my ($filename) = @_;
 
-	open(TODO, "<", "TODO") or die "Couldn't open TODO: $!";
+	my @tasks;
+	open(TODO, "<", $filename) or die "Couldn't open TODO: $!";
+
+	# Read each line.
 	while (my $line = <TODO>) {
 		chomp $line;
 
 		my @part = $line =~ /^(\s[-x]\s)|(.+)$/gi;
-		my $state = " ";
+		my $state = 0;
 		my $msg = $part[3];
 
 		# Check if the task is done.
 		if ($part[0] =~ /x/) {
+			$state = 1;
+		}
+
+		# Create the task hash.
+		my $task = {
+			"done" => $state,
+			"msg"   => $msg
+		};
+
+		# Add the task.
+		push(@tasks, $task);
+	}
+
+	close(TODO);
+	return @tasks;
+}
+
+sub list_tasks {
+	my (@tasks) = @_;
+
+	# Read each line.
+	foreach my $task (@tasks) {
+		my $state = " ";
+		my $msg = $task->{"msg"};
+
+		# Check if the task is done.
+		if ($task->{"done"}) {
 			$state = "x";
 		}
 
 		# Print the task.
 		print "    [$state] $msg\n";
 	}
-	close(TODO);
+}
+
+sub main {
+	my $term = Term::ReadLine->new("tdo");
+	my $prompt = ": ";
+	my $OUT = $term->OUT || \*STDOUT;
+
+	my @tasks = parse_tasks("TODO");
+	list_tasks(@tasks);
+
+	while (defined($_ = $term->readline($prompt))) {
+		my $command = $_;
+
+		if ($command =~ /^(q|quit|exit)$/i) {
+			exit;
+		} elsif ($command =~ /^(d|done)/i) {
+			# Get the arguments.
+			my @args = split(/\s/, $command);
+			shift @args;
+
+			print Dumper(\@args);
+		}
+
+		# Add command to the history if it isn't empty.
+		$term->addhistory($_) if /\S/;
+	}
 }
 
 main();
